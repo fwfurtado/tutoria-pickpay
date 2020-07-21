b50cd33924341806959385c8e826939543c8946c
Rafael Carvalho <crcarvalho@outlook.com>
2020-07-21 18:55:43 -0300

    Transfer use case

---
+++ src/main/java/me/github/fwfurtado/pickpay/exception/NotAllowedOperationException.java
@@ -0,0 +1,7 @@
+package me.github.fwfurtado.pickpay.exception;
+
+public class NotAllowedOperationException extends Exception {
+    public NotAllowedOperationException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/me/github/fwfurtado/pickpay/utils/MathUtils.java
@@ -0,0 +1,12 @@
+package me.github.fwfurtado.pickpay.utils;
+
+import java.math.BigDecimal;
+
+public class MathUtils {
+
+    public static boolean isLessThan(BigDecimal current, BigDecimal value){
+        int comparation = current.compareTo(value);
+        return comparation == -1;
+    }
+
+}
